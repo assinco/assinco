de81bb5b8c67f8b798b8bcfbe551b68baf2c3b71
josedavidnup <josedavidnpx@gmail.com>
2023-07-03 19:07:55 -0400

    assinco card aligntment

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head>
         <meta
-          name="description"
-          content="Jano - Creative Multipurpose React NextJS Template"
+          name='description'
+          content='Assinco Climatizacion - Más de 30 años brindando confort'
         />
         <meta
-          name="keywords"
-          content="agency, bootstrap 5, business, business multipurpose, charity, creative, creative template, crypto, education, hosting, insurance, landing page, portfolio, real estate, responsive, react, nextjs"
+          name='keywords'
+          content='assinco, climatizacion, aire acondicionado, business multipurpose, charity, creative, creative template, crypto, education, hosting, insurance, landing page, portfolio, real estate, responsive, react, nextjs'
         />
 
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/assinco_logo.png' />
       </Head>
       <body>
         <Main />
